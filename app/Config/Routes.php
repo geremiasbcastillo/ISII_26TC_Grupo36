967aa0dbfb56c49b87dd8d4913659e243106585d
Benjamin <geremiasbcastillo@gmail.com>
2026-04-21 20:18:01 -0300

    Soluciono merge

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,11 +13,8 @@ $routes->post('guardar_usuario', 'Usuarios_controller::guardar_usuario');
 
 $routes->get('principal', 'Home::principal');
 
-<<<<<<< HEAD
 $routes->get('registro', 'Home::registro_usuario');
-=======
 
 $routes->get('agregar', 'Equipos_controller::formulario_registro');
 
 $routes->post('registrar_equipo', 'Equipos_controller::guardar');
->>>>>>> 11a3ab7fec398da8330b57a4cf6c11f6321deedc
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -16,15 +16,13 @@ class Home extends BaseController
         return view('plantillas/nav_view', $data) . view('frontend/principal_view', $data) . view('plantillas/footer_view', $data);
     }
 
-<<<<<<< HEAD
     public function registro_usuario() {
         $data['titulo'] = 'Principal';
         return view('plantillas/nav_view', $data) . view('frontend/registro_view', $data) . view('plantillas/footer_view', $data);
-=======
-    public function agregar_equipo()
+    }
+        public function agregar_equipo()
     {
         $data['titulo'] = 'Agregar Equipo';
         return view('plantillas/nav_view', $data) . view('frontend/agregar_equipo_view', $data) . view('plantillas/footer_view', $data);
->>>>>>> 11a3ab7fec398da8330b57a4cf6c11f6321deedc
     }
 }
